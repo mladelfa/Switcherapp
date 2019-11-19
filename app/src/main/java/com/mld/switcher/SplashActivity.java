d506022ff536c0f73df9a4ee0f799880db7af5f1
Matias <ladelfamatias1@gmail.com>
2019-11-18 23:44:16 -0300

    agregando splash screen

--- app/src/main/java/com/mld/switcher/SplashActivity.java
+++ app/src/main/java/com/mld/switcher/SplashActivity.java
@@ -2,12 +2,18 @@ package com.mld.switcher;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class SplashActivity extends AppCompatActivity {
+
+    private ImageView splashImg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        splashImg = findViewById(R.id.splash_img);
+
     }
 }
